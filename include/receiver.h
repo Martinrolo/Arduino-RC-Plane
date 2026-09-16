67c56da7039943c00de69a68f4f8478725c3b9b4
Martin Rolo Dussault <martinrolod@hotmail.com>
2026-09-16 13:47:41 -0400

    feat : add smoother changes to control values

--- include/receiver.h
+++ include/receiver.h
@@ -2,7 +2,9 @@
 #define RECEIVER_H
 
 void receiveData();
+void resetData();
 void writeData();
+uint8_t setNextServoValue(uint8_t target, uint8_t current, int16_t step);
 void lostTransmissionHandler();
 
 #endif
